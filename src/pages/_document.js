3530b24525e063935d5e65a8b923d16aef947e70
sly <sylvaincadoret@hotmail.com>
2026-02-05 16:44:08 +0100

    feat: update font styles across components and templates for consistency

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -27,31 +27,27 @@ export default function Document() {
         <link rel="icon" href="" />
 
         {/*  External Stylesheets */}
+
         <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-        />
-        <link
-          href="https://fonts.cdnfonts.com/css/quicksand"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        ></link>
+
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         />
-        <link href="https://fonts.cdnfonts.com/css/roboto" rel="stylesheet" />
-
-        <link href="https://fonts.cdnfonts.com/css/poppins" rel="stylesheet" />
-
+        {/* Google Fonts - Poppins & Droid Serif */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
         />
-     
-
-<link href="https://fonts.cdnfonts.com/css/poppins" rel="stylesheet" />
-<link href="https://fonts.cdnfonts.com/css/droid-serif" rel="stylesheet" />
-
 
         {/* Google Tag Manager */}
       </Head>
