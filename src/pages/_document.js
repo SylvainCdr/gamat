91bb07143bedc5257af266e4eeb5d552db90e689
sly <sylvaincadoret@hotmail.com>
2026-04-27 14:22:02 +0200

    Refactor code structure for improved readability and maintainability

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,9 @@ export default function Document() {
         <meta property="og:site_name" content="GAMAT" />
 
         {/* Favicon */}
-        <link rel="icon" href="/assets/logos/favicon.ico" />
+        <link rel="icon" href="/assets/logos/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/logos/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/logos/favicon-16x16.png" />
 
         {/*  External Stylesheets */}
 
